66f416ae89a91f590582eec545a6fae0e199edf4
abin98anto <abin98anto@gmail.com>
2024-12-04 21:17:22 +0530

    UserSchem.ts Added

---
+++ src/infrastructure/database/mongoose-schemas/UserSchema.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { UserRole } from "../../../core/entities/User";
+import { SubscriptionType } from "../../../core/entities/User";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: Object.values(UserRole),
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+  },
+  subscription: {
+    type: String,
+    enum: Object.values(SubscriptionType),
+    required: true,
+  },
+  dateJoined: {
+    type: Date,
+    default: Date.now,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export const UserModel = mongoose.model("User", userSchema);
