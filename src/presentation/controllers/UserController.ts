1d6fcec3411c2a1a94ea515a4f8c0f360e89e69a
abin98anto <abin98anto@gmail.com>
2024-12-05 11:06:44 +0530

    Added userRoutes.ts -Made changes to the user routes. -Made changes to the controller.

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -14,28 +14,6 @@ const tempOTPStore: {
   [email: string]: { otp: string; expiresAt: Date; userData: User };
 } = {};
 
-// export class UserController {
-//   private addUserUseCase: AddUserUseCase;
-
-//   constructor() {
-//     const userRepository = new UserRepository();
-//     this.addUserUseCase = new AddUserUseCase(userRepository);
-//   }
-
-//   async register(req: Request, res: Response): Promise<void> {
-//     try {
-//       const user = await this.addUserUseCase.execute(req.body);
-//       res.status(201).json(user);
-//     } catch (error) {
-//       if (error instanceof Error) {
-//         res.status(400).json({ message: error.message });
-//       } else {
-//         res.status(400).json({ message: messages.UNKNOWN_ERROR });
-//       }
-//     }
-//   }
-// }
-
 export class UserController {
   constructor(
     private sendOTPUseCase: SendOTPUseCase,
@@ -75,33 +53,4 @@ export class UserController {
       }
     }
   }
-
-  // async register(req: Request, res: Response): Promise<void> {
-  //   const { name, email, password, profilePicture } = req.body;
-  //   const userData: User = {
-  //     name: name,
-  //     email: email,
-  //     password: password,
-  //     profilePicture: profilePicture,
-  //     role: UserRole.USER,
-  //     subscription: SubscriptionType.FREE,
-  //     dateJoined: new Date(),
-  //     isActive: true,
-  //   };
-
-  //   try {
-  //     const { otp, expiresAt } = generateOTP();
-  //     tempOTPStore[email] = { otp, expiresAt, userData };
-
-  //     await this.emailService.sendOTP(email, otp);
-
-  //     res.status(200).json({ message: otpErrors.OTP_SENT, email });
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       res
-  //         .status(500)
-  //         .json({ message: otpErrors.OTP_SENDING_ERROR, error: error.message });
-  //     }
-  //   }
-  // }
 }
--- src/presentation/routes/userRoutes.ts
+++ src/presentation/routes/userRoutes.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { UserController } from "../controllers/UserController";
+import { UserRepository } from "../../infrastructure/repositories/UserRepository";
+import { EmailService } from "../../infrastructure/external-services/EmailService";
+import { SendOTPUseCase } from "../../core/use-cases/user/SendOTPUseCase";
+import { VerifyOTPUseCase } from "../../core/use-cases/user/VerifyOTPUseCase";
 
 const router = express.Router();
-const userController = new UserController();
 
-router.post("/signup", userController.register.bind(userController));
+const userRepository = new UserRepository();
+const emailService = new EmailService();
+const sendOTPUseCase = new SendOTPUseCase(userRepository, emailService);
+const verifyOTPUseCase = new VerifyOTPUseCase(userRepository);
+const userController = new UserController(sendOTPUseCase, verifyOTPUseCase);
+
+router.post("/send-otp", (req, res) => userController.sendOTP(req, res));
+router.post("/verify-otp", (req, res) => userController.verifyOTP(req, res));
 
 export default router;
