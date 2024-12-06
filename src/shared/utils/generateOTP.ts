26c6930fa0ea32faed4fc1d1fd4b97e35f4e018c
abin98anto <abin98anto@gmail.com>
2024-12-06 14:40:32 +0530

    Made otp expiration 5 minutes

--- src/shared/utils/generateOTP.ts
+++ src/shared/utils/generateOTP.ts
@@ -1,5 +1,5 @@
 export const generateOTP = (length = 6): { otp: string; expiresAt: Date } => {
   const otp = Math.random().toString().slice(-length);
-  const expiresAt = new Date(Date.now() + 10 * 1000);
+  const expiresAt = new Date(Date.now() + 5 * 10 * 1000);
   return { otp, expiresAt };
 };
