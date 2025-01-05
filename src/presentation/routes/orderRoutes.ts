a67c148540206ca408b0137fd9b1ec6469697241
abin98anto <abin98anto@gmail.com>
2025-01-05 20:35:58 +0530

    -Order Routes created.

---
+++ src/presentation/routes/orderRoutes.ts
@@ -0,0 +1,25 @@
+import { OrderRepositoryInterface } from "../../core/interfaces/OrderRepositoryInterface";
+import { CreateOrderUseCase } from "../../core/use-cases/order/CreateOrderUseCase";
+import { GetAllOrderUseCase } from "../../core/use-cases/order/GetAllOrderUseCase";
+import { GetUserOrdersUseCase } from "../../core/use-cases/order/GetUserOrdersUseCase";
+import { OrderRepository } from "../../infrastructure/repositories/OrderRepository";
+import { OrderManagementController } from "../controllers/orderController/orderManagementController";
+import express from "express";
+
+const orderRouter = express.Router();
+
+const orderRepository: OrderRepositoryInterface = new OrderRepository();
+const createOrderUseCase = new CreateOrderUseCase(orderRepository);
+const getAllOrdersUseCase = new GetAllOrderUseCase(orderRepository);
+const getUserOrdersUseCase = new GetUserOrdersUseCase(orderRepository);
+const orderManagementController = new OrderManagementController(
+  createOrderUseCase,
+  getAllOrdersUseCase,
+  getUserOrdersUseCase
+);
+
+orderRouter.post("/create-order", orderManagementController.addOrder);
+orderRouter.get("/all-orders", orderManagementController.getAllOrders);
+orderRouter.get("/user-orders", orderManagementController.getUserOrders);
+
+export default orderRouter;
